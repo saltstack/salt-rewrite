fd9b6c2f0d60977e64916d8cafd5f890eec2a31d
Pedro Algarvio <pedro@algarvio.me>
2020-02-01 16:26:10 +0000

    Automate the `__all__` generation with support for priorities

--- src/saltrewrite/testsuite/__init__.py
+++ src/saltrewrite/testsuite/__init__.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
-from saltrewrite.testsuite import fix_destructive_test_decorator
+import operator
 
-__all__ = [modname for modname in list(locals()) if modname.startswith("fix_")]
+from saltrewrite.testsuite import fix_destructive_test_decorator
+from saltrewrite.testsuite import fix_expensive_test_decorator
+
+__all__ = [
+    module.__name__
+    for (priority, module) in sorted(
+        [
+            (getattr(module, "FIX_PRIORITY", 0), module)
+            for module in [
+                globals()[modname] for modname in list(globals()) if modname.startswith("fix_")
+            ]
+        ],
+        key=operator.itemgetter(0),
+    )
+]
