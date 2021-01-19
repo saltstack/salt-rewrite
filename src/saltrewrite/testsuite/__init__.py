946c86d944b880b407d95207e126d4cba05e761c
Pedro Algarvio <pedro@algarvio.me>
2021-01-19 14:12:30 +0000

    Add missing import

--- src/saltrewrite/testsuite/__init__.py
+++ src/saltrewrite/testsuite/__init__.py
@@ -3,6 +3,7 @@
 from saltrewrite.testsuite import fix_asserts
 from saltrewrite.testsuite import fix_destructive_test_decorator
 from saltrewrite.testsuite import fix_expensive_test_decorator
+from saltrewrite.testsuite import fix_slow_test_decorator
 
 __all__ = [modname for modname in list(globals()) if modname.startswith("fix_")]
 
