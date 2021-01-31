89a6eafb6c0c4cc857a288993a106195ec93df79
Pedro Algarvio <pedro@algarvio.me>
2021-01-31 09:04:55 +0000

    Import the additional decorator fixes

--- src/saltrewrite/testsuite/__init__.py
+++ src/saltrewrite/testsuite/__init__.py
@@ -3,6 +3,11 @@
 from saltrewrite.testsuite import fix_asserts
 from saltrewrite.testsuite import fix_destructive_test_decorator
 from saltrewrite.testsuite import fix_expensive_test_decorator
+from saltrewrite.testsuite import fix_requires_network_decorator
+from saltrewrite.testsuite import fix_requires_salt_modules_decorator
+from saltrewrite.testsuite import fix_requires_salt_states_decorator
+from saltrewrite.testsuite import fix_skip_if_binaries_missing_decorator
+from saltrewrite.testsuite import fix_skip_if_not_root_decorator
 from saltrewrite.testsuite import fix_slow_test_decorator
 
 __all__ = [modname for modname in list(globals()) if modname.startswith("fix_")]
